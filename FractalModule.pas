59cf0a695d4b56057a106d932bc5db9f82c63ffd
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2024-03-04 16:27:36 +0300

    Add files via upload

---
+++ FractalModule.pas
@@ -0,0 +1,37 @@
+﻿unit FractalModule;
+
+interface
+uses graphABC;
+const
+  depth = 5; // глубина прорисовки
+procedure DrawCovrik(x, y, size, depth: integer);
+implementation
+procedure DrawCover(x, y, size, depth: integer); // процедура для рекурсивного построения фрактала
+begin
+  if depth = 0 then
+    exit
+  else
+  begin
+    Pen.Color := clBlack;
+    Rectangle(x, y, x + size, y + size);
+    size := size div 3;
+
+    DrawCover(x, y, size, depth - 1);
+    DrawCover(x + size, y, size, depth - 1);
+    DrawCover(x + 2 * size, y, size, depth - 1);
+    
+    DrawCover(x, y + size, size, depth - 1);
+    DrawCover(x + 2 * size, y + size, size, depth - 1);
+    
+    DrawCover(x, y + 2 * size, size, depth - 1);
+    DrawCover(x + size, y + 2 * size, size, depth - 1);
+    DrawCover(x + 2 * size, y + 2 * size, size, depth - 1);
+  end;
+end;
+
+procedure DrawCovrik(x, y, size, depth: integer);
+begin
+  DrawCover(x, y, size, depth);
+end;
+
+end.
---
+++ Programm.pas
@@ -0,0 +1,34 @@
+﻿uses FractalModule, graphABC, Controls;
+var DEPTH: Integer = 5; // Глубина фрактала
+FractalX, FractalY, FractalSize: Integer;
+const MOVE_STEP = 10;
+const ZOOM_STEP = 20;
+const START_SIZE = 500;
+
+// Обработчик событий клавиатуры
+Procedure KeyDown(Key: Integer);
+begin
+  case Key of
+    VK_LEFT: FractalX := FractalX - MOVE_STEP; // Продвигаемся влево
+    VK_RIGHT: FractalX := FractalX + MOVE_STEP; // Продвигаемся вправо
+    VK_UP: FractalY := FractalY - MOVE_STEP; // Продвигаемся вверх
+    VK_DOWN: FractalY := FractalY + MOVE_STEP; // Продвигаемся вниз
+    VK_SUBTRACT: FractalSize := FractalSize - ZOOM_STEP; // Уменьшаем размер
+    VK_ADD: FractalSize := FractalSize + ZOOM_STEP; // Увеличиваем размер
+  end; 
+  ClearWindow; // Очищаем окно перед перерисовкой
+  DrawCovrik(FractalX, FractalY, FractalSize, DEPTH); // Перерисовываем с новыми параметрами
+end;
+
+// Главная часть программы
+begin
+  FractalX := 50;
+  FractalY := 50;
+  FractalSize := START_SIZE;
+
+  SetWindowCaption('Фрактал Ковер Серпинского');
+  SetWindowSize(800, 800);
+  OnKeyDown := KeyDown; // Устанавливаем обработчик событий клавиатуры
+  { Рисуем фрактал Ковра Серпинского с начальными параметрами }
+  DrawCovrik(FractalX, FractalY, FractalSize, DEPTH);
+end.
